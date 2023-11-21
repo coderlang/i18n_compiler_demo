84cb4533a1ce8c13d82f6219ed97ecbec9a84547
coderlang <coderlang@foxmail.com>
2023-11-21 18:44:13 +0800

    更新 git 配置

--- android/init.sh
+++ android/init.sh
@@ -2,4 +2,7 @@
 
 root="$(pwd)"
 
+git config --global pull.rebase true
+git config --global submodule.recurse true
+
 cd submodules/i18n_compiler/bin && ./i18n_compiler android "$root/submodules/i18n_compiler/test/i18n.csv" "$root/res/"
