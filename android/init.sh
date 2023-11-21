1181a4b2e8e7b5151e5b6caead2567b4b519468a
coderlang <coderlang@foxmail.com>
2023-11-21 18:54:23 +0800

    更新安卓

--- android/init.sh
+++ android/init.sh
@@ -5,4 +5,4 @@ root="$(pwd)"
 git config --global pull.rebase true
 git config --global submodule.recurse true
 
-cd submodules/i18n_compiler/bin && ./i18n_compiler android "$root/submodules/i18n_compiler/test/i18n.csv" "$root/res/"
+cd submodules/i18n_compiler/bin && ./i18n_compiler android "$root/submodules/i18n_language_demo/i18n.csv" "$root/res/"
